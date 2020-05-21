7fb3e8348443f65d986a162f6daa12d4c18a0036
Djeckkenni <djeck27kenni@gmail.com>
2020-05-21 10:38:54 +0300

    correct code

--- src/main/java/com/dev/cinema/Main.java
+++ src/main/java/com/dev/cinema/Main.java
@@ -5,7 +5,7 @@ import com.dev.cinema.model.Movie;
 import com.dev.cinema.service.MovieService;
 
 public class Main {
-    private static Injector INJECTOR = Injector.getInstance("com.dev.cinema");
+    private static final Injector INJECTOR = Injector.getInstance("com.dev.cinema");
 
     public static void main(String[] args) {
         Movie movie = new Movie();
--- src/main/java/com/dev/cinema/util/HibernateUtil.java
+++ src/main/java/com/dev/cinema/util/HibernateUtil.java
@@ -4,7 +4,7 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
-    private static final SessionFactory SESSION_FACTORY = initSessionFactory();
+    private static SessionFactory sessionFactory = initSessionFactory();
 
     private HibernateUtil() {
     }
@@ -18,7 +18,7 @@ public class HibernateUtil {
     }
 
     public static SessionFactory getSessionFactory() {
-        return SESSION_FACTORY;
+        return sessionFactory;
     }
 }
 
