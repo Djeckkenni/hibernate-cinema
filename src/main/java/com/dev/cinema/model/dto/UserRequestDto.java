da9a9b3416210f4700b69ce775f282be1acaf44a
Djeckkenni <djeck27kenni@gmail.com>
2020-06-16 11:38:33 +0300

    added-solution

--- src/main/java/com/dev/cinema/model/dto/UserRequestDto.java
+++ src/main/java/com/dev/cinema/model/dto/UserRequestDto.java
@@ -1,8 +1,26 @@
 package com.dev.cinema.model.dto;
 
+import com.dev.cinema.annotation.EmailConstraint;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class UserRequestDto {
+    @NotNull(message = "Email can't be null!")
+    @EmailConstraint
     private String email;
+    @NotNull(message = "Password can't be null!")
+    @Size(min = 8, message = "Number of symbols of password must be greater or equal 8!")
     private String password;
+    @NotNull(message = "Repeat password can't be null!")
+    private String repeatPassword;
+
+    public String getRepeatPassword() {
+        return repeatPassword;
+    }
+
+    public void setRepeatPassword(String repeatPassword) {
+        this.repeatPassword = repeatPassword;
+    }
 
     public String getEmail() {
         return email;
