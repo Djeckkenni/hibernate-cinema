f96bd8b1feafaf801646cbbb687f39c016ec10ca
Djeckkenni <djeck27kenni@gmail.com>
2020-06-19 08:48:58 +0300

    added-solution

--- src/main/java/com/dev/cinema/security/AuthenticationServiceImpl.java
+++ src/main/java/com/dev/cinema/security/AuthenticationServiceImpl.java
@@ -1,26 +1,34 @@
 package com.dev.cinema.security;
 
 import com.dev.cinema.exception.AuthenticationException;
+import com.dev.cinema.model.Role;
 import com.dev.cinema.model.User;
+import com.dev.cinema.service.RoleService;
 import com.dev.cinema.service.UserService;
-import com.dev.cinema.util.HashUtil;
+import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
     private final UserService userService;
+    private final RoleService roleService;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public AuthenticationServiceImpl(UserService userService) {
+    public AuthenticationServiceImpl(UserService userService,
+                                     RoleService roleService, PasswordEncoder passwordEncoder) {
         this.userService = userService;
+        this.roleService = roleService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public User login(String email, String password) throws AuthenticationException {
         User user = userService.findByEmail(email);
         if (user != null
-                && HashUtil.hashPassword(password, user.getSalt()).equals(user.getPassword())) {
+                && passwordEncoder.encode(user.getPassword()).equals(user.getPassword())) {
             return user;
         }
         throw new AuthenticationException("Incorrect email or password!!!");
@@ -29,9 +37,9 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     @Override
     public User register(String email, String password) {
         User user = new User();
-        user.setEmail(email);
-        user.setSalt(HashUtil.getSalt());
-        user.setPassword(HashUtil.hashPassword(password, user.getSalt()));
+        user.setPassword(passwordEncoder.encode(password));
+        Role role = roleService.getRoleByName("USER");
+        user.setRoles(Set.of(role));
         return userService.add(user);
     }
 }
