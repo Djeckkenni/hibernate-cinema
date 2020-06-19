01a2466312e7212c4d344acb80043cf90d466d33
Djeckkenni <djeck27kenni@gmail.com>
2020-06-19 08:48:58 +0300

    added-solution

--- src/main/java/com/dev/cinema/model/User.java
+++ src/main/java/com/dev/cinema/model/User.java
@@ -1,10 +1,12 @@
 package com.dev.cinema.model;
 
+import java.util.Set;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class User {
@@ -14,7 +16,8 @@ public class User {
     @Column(unique = true)
     private String email;
     private String password;
-    private byte[] salt;
+    @ManyToMany
+    private Set<Role> roles;
 
     public Long getId() {
         return id;
@@ -40,20 +43,19 @@ public class User {
         this.password = password;
     }
 
-    public byte[] getSalt() {
-        return salt;
+    public Set<Role> getRoles() {
+        return roles;
     }
 
-    public void setSalt(byte[] salt) {
-        this.salt = salt;
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
     }
 
     @Override
     public String toString() {
-        return "User{"
-                + "id=" + id
-                + ", email='" + email + '\''
-                + ", password='" + password + '\''
-                + '}';
+        return "User{" + "id=" + id + ", email='"
+                + email + '\'' + ", password='"
+                + password + '\'' + ", roles="
+                + roles + '}';
     }
 }
